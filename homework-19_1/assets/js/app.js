4ce4cadce79ae26288f66793ba2d373070fd2b10
Vladislav9724 <vlad.tsyapka@gmail.com>
2022-04-27 10:44:50 +0300

    Update app.js

--- homework-19_1/assets/js/app.js
+++ homework-19_1/assets/js/app.js
@@ -13,15 +13,15 @@ console.log(`Індекс: ${index}`);
 
 if(index <= 16){
     console.log(`${index} - виражений дефіцит маси тіла. Норма (16 та менше)`);
-}else if(16 > index < 18.5){
+}else if(index >= 16 && index <= 18.5){
     console.log(`${index} - недостотня маси тіла. Норма (16 - 18.5)`);
-}else if(18.5 > index < 25){
+}else if(index >= 18.5 && index <= 25){
     console.log(`${index} - норма. Норма (18.5 - 25)`);
-}else if(25 > index < 30){
+}else if(index >= 25 && index <= 30){
     console.log(`${index} - надлишккава маса тіла. Норма (25 - 30)`);
-}else if(30 < index < 35){
+}else if(index >= 30 && index <= 35){
     console.log(`${index} - ожиріння 1 категорії. Норма (30 - 35)`);
-}else if(35 < index < 40){
+}else if(index >= 35 && index <= 40){
     console.log(`${index} - ожиріння 2 категорії. Норма (35 - 40)`);
 }else if(index >= 40){
     console.log(`${index} - ожиріння 3 категорії. Норма (40 та більше)`);
